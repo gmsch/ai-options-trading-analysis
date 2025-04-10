aed0a924d12970480565cdb453c8434ecc2461ec
Gary Schneider <gary@goodworkstechnology.com>
2025-04-10 10:28:05 -0400

    Create data_acquisition.py
    Initial file creation as defined in README

---
+++ data_acquisition.py
@@ -0,0 +1,9 @@
+def fetch_historical_stock_data(symbol, start_date, end_date):
+    """ 
+    Fetch historical stock data from financial APIs.
+    """
+
+def fetch_options_data(symbol, expiration_date):
+    """
+    Retrieve historical or current options data for given symbol and expiration.
+    """
