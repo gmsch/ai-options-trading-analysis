3aa49b0b73b5c4f7bf43560984d515d43d04cab0
Gary Schneider <gary@goodworkstechnology.com>
2025-04-10 10:31:20 -0400

    Create visualizer.py
    Initial file creation

---
+++ visualizer.py
@@ -0,0 +1,5 @@
+def plot_stock_chart_with_indicators(stock_data, indicators):
+    """Visualize stock price along with key indicators."""
+
+def display_trade_signals(symbol, stock_data, recommendations):
+    """Clearly visualize trade recommendations alongside charts."""
