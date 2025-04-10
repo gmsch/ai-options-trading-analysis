12865cff8344cca61adbc91a68c96f3ec2d9a949
Gary Schneider <gary@goodworkstechnology.com>
2025-04-10 10:29:05 -0400

    Create data_processing.py
    Initial file creation

---
+++ data_processing.py
@@ -0,0 +1,9 @@
+def clean_and_process_stock_data(raw_stock_data):
+    """
+    Clean, handle missing data, and standardize data format.
+    """
+
+def generate_features(cleaned_data):
+    """
+    Generate predictive input features from processed historical data.
+    """
